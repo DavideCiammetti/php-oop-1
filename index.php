d26a289e96db67f934751866bca36fd2cbe67546
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-15 14:56:24 +0100

    fix: creo classe movie e istanzo 2 oggetti della classe.

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
 <?php
+
+class Movie {
+    public $nome;
+    public $genere ;
+    public $durata;
+
+    function __construct($genere){
+        $this->genere = 'genere:'."\t".$genere;
+    }
+
+    public function setDurata($durata){
+        if($durata >= 180){
+            echo 'il film è impegnativo dura '. $durata;
+        }else{
+            echo 'durata:'."\t".$durata;
+        }
+    }
+}
+// primo film
+echo 'primo film'.'<br>';
+$movieOne = new Movie('fantasy');
+
+echo $movieOne->nome = 'film:'."\t".'lord of the rings'.'<br>';
+echo $movieOne->genere.'<br>';
+echo $movieOne->setDurata(180);
+
+echo '<br>';
+echo '<br>';
+// secondo film
+echo 'secondo film'.'<br>';
+$movieTwo = new Movie('sci-fi');
+
+echo $movieTwo->nome = 'film:'."\t".'back to the future'.'<br>';
+echo $movieTwo->genere.'<br>';
+echo $movieTwo->setDurata(120);
+
+?>
