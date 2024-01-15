a0e91cf92d1ea900da8657ee3dcbe7359c43b4ed
DavideCiammetti <davideciammetti97@gmail.com>
2024-01-15 16:01:07 +0100

    fix: scaffolding spostamento classe

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ reate un file index.php in cui:
     - all'interno della classe è definito almeno un metodo
 2. vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
 3. Modificare la classe Movie in modo che accetti più di un genere
+4. Creare un layout completo per stampare a schermo una lista di movies suddividendolo in appositi file e cartelle
+mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare Models
+organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
+
+film => nome, durata 
+^
+|---> genere, anno uscita
--- models/movies.php
+++ models/movies.php
@@ -2,12 +2,7 @@
 
 class Movie {
     public $nome;
-    public $genere ;
     public $durata;
-
-    function __construct(array $genere){
-        $this->genere = $genere;
-    }
 
     public function setDurata($durata){
         if($durata >= 180){
@@ -19,20 +14,18 @@ class Movie {
 }
 // primo film
 echo 'primo film'.'<br>';
-$movieOne = new Movie(['fantasy', 'adventure']);
+$movieOne = new Movie();
 
 echo $movieOne->nome = 'film:'."\t".'lord of the rings'.'<br>';
-var_dump($movieOne->genere);
 echo $movieOne->setDurata(180);
 
 echo '<br>';
 echo '<br>';
 // secondo film
 echo 'secondo film'.'<br>';
-$movieTwo = new Movie(['sci-fi', 'action']);
+$movieTwo = new Movie();
 
 echo $movieTwo->nome = 'film:'."\t".'back to the future'.'<br>';
-var_dump($movieTwo->genere);
 echo $movieTwo->setDurata(120);
 
 ?>
